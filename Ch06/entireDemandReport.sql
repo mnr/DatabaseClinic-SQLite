d24965cf1c85d4b72aeea1fc7f68f91014d498f8
Mark Niemann-Ross <mark.niemannross@gmail.com>
2017-07-20 17:29:27 -0700

    Using UNION to combine demand tables

---
+++ Ch06/entireDemandReport.sql
@@ -0,0 +1,16 @@
+SELECT substr(Year,7,4) AS Year, EducationalAttainment, PopulationCount AS Demand
+FROM 'CA_Educational_Attainment___Personal_Income_2008-2014'
+GROUP BY Year,EducationalAttainment
+
+UNION
+
+SELECT Year, EducationalAttainment, CAST(sum(coefficient * Population) AS INTEGER) AS Demand
+FROM 'CA_DRU_proj_2010-2060' DRU
+JOIN demographicSplit ON DRU.Gender = demographicSplit.Gender
+    AND demographicSplit.Age = CASE
+		      WHEN CAST (DRU.Age as INTEGER) < 18 THEN '00 to 17'
+			  WHEN CAST (DRU.Age as INTEGER) < 65 THEN '18 to 64'
+			  ELSE '65 to 80+'
+		END
+WHERE CAST(Year AS INTEGER)  > 2014
+GROUP BY Year, EducationalAttainment
