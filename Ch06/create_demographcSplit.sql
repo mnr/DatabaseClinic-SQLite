12171379e0bea0ed291f360ae59ce7cb4138e16d
Mark Niemann-Ross <mark.niemannross@gmail.com>
2017-07-21 17:01:49 -0700

    added caea and subquery

--- Ch06/create_demographcSplit.sql
+++ Ch06/create_demographcSplit.sql
@@ -1,8 +1,12 @@
-CREATE TABLE demographicSplit AS 
- SELECT Age, Gender, EducationalAttainment,
-    sum(PopulationCount) / 
-      (SELECT sum(PopulationCount)
-	     FROM 'CA_Educational_Attainment___Personal_Income_2008-2014'
-	     GROUP BY Age, Gender) AS coefficient
-    FROM 'CA_Educational_Attainment___Personal_Income_2008-2014'
-    GROUP BY Age,Gender,EducationalAttainment
+CREATE TABLE demographicSplit AS
+SELECT CAEA.Age, CAEA.Gender, EducationalAttainment,  
+      	sum(PopulationCount) / lookup.TotalPopCount AS coefficient
+FROM 'CA_Educational_Attainment___Personal_Income_2008-2014' CAEA
+JOIN 
+   (SELECT Age, Gender, CAST(sum(PopulationCount) as REAL) AS TotalPopCount
+   FROM 'CA_Educational_Attainment___Personal_Income_2008-2014'
+   GROUP BY Age, Gender) AS lookup 
+   ON 
+     CAEA.Age = lookup.Age 
+	 AND CAEA.Gender = lookup.Gender
+GROUP BY CAEA.Age,CAEA.Gender,EducationalAttainment
